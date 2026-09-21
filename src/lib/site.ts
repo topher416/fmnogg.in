432b43d36b41047586ec133af9139c74179bee35
Topher Rasmussen <topher416@gmail.com>
2026-09-21 01:30:34 -0500

    Add confirmed Sept 25 show details (time, cover, 21+, full bill, FB event link)

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -10,8 +10,16 @@ export const BAND = {
 
 export const SHOW = {
   venue: "Montrose Saloon",
+  address: "2933 W Montrose Ave, Chicago, IL 60618",
   city: "Chicago, IL",
-  date: "September 25, 2026",
+  date: "Friday, September 25, 2026",
   dateShort: "Sep 25",
-  support: "with The Blue You Once Knew (Pink Floyd tribute)",
+  // Confirmed from the Montrose Saloon Facebook event (Sep 21, 2026).
+  start: "7:30 PM",
+  cover: "$15 at the door",
+  age: "21+ w/ physical I.D.",
+  // Full bill: Side Door 7:30, The Blue You Once Knew 8:30, Krononaut 9:30.
+  support: "with The Blue You Once Knew (Pink Floyd tribute), Krononaut & Side Door",
+  setTime: "10:30 PM",
+  eventUrl: "https://www.facebook.com/events/849340254835965/",
 };
